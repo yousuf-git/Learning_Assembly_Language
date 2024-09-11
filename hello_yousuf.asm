3b5afa122677eb877894f4c709d36ca87e725d2a
yousuf-git <hackerbest052@gmail.com>
2024-09-11 18:19:01 +0500

    Added comments

--- hello_yousuf.asm
+++ hello_yousuf.asm
@@ -1,13 +1,15 @@
-.data
+# Simple program to print "Hello Yousuf" on the console
+
+.data           # Data segment
 
 hello_msg: .asciiz "Hello Yousuf\n"
 
-.text
+.text           # Text segment
 
-main:
-    li $v0, 4
-    la $a0, hello_msg
-    syscall
+main:           # Main function
+    li $v0, 4                   # Load the print_string syscall code
+    la $a0, hello_msg           # Load the address of the string to print
+    syscall                     # Call the print_string syscall
 
-    li $v0, 10
-    syscall
+    li $v0, 10                  # Load the exit syscall code
+    syscall                     # Call the exit syscall
