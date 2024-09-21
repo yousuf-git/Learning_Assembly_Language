4517f25b4c4dae8a51cf65b1f4193f473967278a
yousuf-git <hackerbest052@gmail.com>
2024-09-21 17:39:20 +0500

    Modified: Existing programs into proper template

--- hello_yousuf.asm
+++ hello_yousuf.asm
@@ -1,15 +1,26 @@
-# Simple program to print "Hello Yousuf" on the console
+# --------------- Program Intro ---------------- #
 
-.data           # Data segment
+# Title: To print "Hello Yousuf" on the console
+# Author: Muhammad Yousuf 
+# Description: Displays "Hello Yousuf" on the console
+# Date: Aug 2024
+# INPUT: No input
+# OUTPUT: "Hello Yousuf" on the console
 
-hello_msg: .asciiz "Hello Yousuf\n"
+# --------------- Data Segement / Variables ---------------- #
 
-.text           # Text segment
+.data
+    hello_msg:	.asciiz "Hello Yousuf\n"	# string to be printed
 
-main:           # Main function
-    li $v0, 4                   # Load the print_string syscall code
-    la $a0, hello_msg           # Load the address of the string to print
-    syscall                     # Call the print_string syscall
+.global		main 					# optional
 
-    li $v0, 10                  # Load the exit syscall code
-    syscall                     # Call the exit syscall
+# --------------- Code Segement / Instructions ---------------- #
+
+.text
+main:
+    li $v0, 4					# syscall code for print_string
+    la $a0, hello_msg			# load address of the string to print
+    syscall						# call the print_string syscall
+
+    li $v0, 10					# syscall code for exit
+    syscall						# call the exit syscall
--- sumOfTwoInt.asm
+++ sumOfTwoInt.asm
@@ -1,24 +1,37 @@
-# adding two variables and printing sum on cosole
+# --------------- Program Intro ---------------- #
+
+# Title: 				Sum of Two Integers
+# Author: 				Muhammad Yousuf
+# Description: 			To calculate the sum of two integers
+# Date: 				Sep 2024
+# INPUT: 				Two integers
+# OUTPUT: 				Sum of the two integers
+
+# --------------- Data Segement / Variables ---------------- #
 
 .data
-	n1 : .word	60
-	n2 : .word	9
-	
+	n1 : .word	60				# First Integer containing 4 bytes
+	n2 : .word	9				# Second Integer
+
+.global		main 					# optional
+# --------------- Code Segement / Instructions ---------------- #
+
 .text
 
-.main:
-	# loading values of n1 and n2 in 2 registers
+main:
+	# loading values of n1 and n2 in two temporary registers
 	lw $t0, n1	
 	lw $t1, n2
 	
 	# Add values in 2 registers and store in any of them or third one
-	add $t0, $t0, $t1
+	add $t0, $t0, $t1			# Adding values of $t0 and $t1 and storing in $t0
 	
 	# Now $t0 holds the result
 	
-	li $v0, 1	# Load system call to print integer
-	move $a0, $t0   # Store value of result in $a0
+	li $v0, 1					# Load system call to print integer
+	move $a0, $t0   			# Store value of result in $a0
 	syscall
 	
-	li $v0, 10		# Loading exit system call
-	syscall			# make call
+	li $v0, 10					# Loading exit system call
+	syscall						# make system call
+
